98cdeff71776ab157e9315dea1348fee94dd290a
Nafizulhaque535 <codeglobal35@gmail.com>
2020-07-17 04:54:29 +0600

    submitting solve

--- assignment.js
+++ assignment.js
@@ -5,3 +5,35 @@ function feetToMile(feet){
 }
 var convert = feetToMile(1000);
 console.log(convert);
+
+
+
+//woodCalculator
+function woodCalculator(chair, table, bed){
+    var chairCount = chair * 1;
+    var tableCount = table * 3;
+    var bedCount = bed * 5;
+
+    var totalWood = chairCount + tableCount + bedCount;
+    return totalWood;
+}
+var woodResult = woodCalculator(10, 15, 20);
+console.log(woodResult);
+
+
+//brickCalculator
+//tinyFriend
+function tinyFriend(names){
+    var youngest = names[0];
+
+    for(var i = 0; i < names.length; i++){
+        var currentName = names[i];
+
+        if(currentName < youngest){
+            youngest = currentName;
+        }
+    }
+    return youngest;
+}
+var youngestName = tinyFriend(["Messi", "Ronaldo" , "Kaka", "Nafiz"]);
+console.log(youngestName);
